589a3ee02a4f4c5e315b64a6468537032c45e475
Aaditya Chaudhari <115763672+aadi1720@users.noreply.github.com>
2023-09-26 14:04:51 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,14 +5,14 @@ from unittest.mock import patch
 
 from contacts import *
 
-class Test01_AddContact(unittest.TestCase):
+class Test02_AddContact(unittest.TestCase):
     def test_list_int(self):
         """
-        *** Test01 *** GIVEN: c = {7145551111: ['Steve', 'Jobs'], 5625553333: ['Bill', 'Gates']} *** FUNCTION CALL: add_contact(c, id = 7145551212, first_name = 'Richard', last_name = 'Stallman') *** EXPECT: c = {7145551111: ['Steve', 'Jobs'], 5625553333: ['Bill', 'Gates'], 7145551212: ['Richard', 'Stallman']} ***
+        *** Test02 *** GIVEN: c = {7145551111: ['Steve', 'Jobs'], 5625553333: ['Bill', 'Gates']} *** FUNCTION CALL: r = add_contact(c, id = 7145551111, first_name = 'Richard', last_name = 'Stallman') *** EXPECT: r = 'error' ***
         """
         c = {7145551111: ['Steve', 'Jobs'], 5625553333: ['Bill', 'Gates']}
-        add_contact(c, id = 7145551212, first_name = 'Richard', last_name = 'Stallman')
-        self.assertEqual(c, {7145551111: ['Steve', 'Jobs'], 5625553333: ['Bill', 'Gates'], 7145551212: ['Richard', 'Stallman']})
+        r = add_contact(c, id = 7145551111, first_name = 'Richard', last_name = 'Stallman')
+        self.assertEqual(r, 'error')
         print()
 
 
